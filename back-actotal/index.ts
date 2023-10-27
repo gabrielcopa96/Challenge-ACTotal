23eb6b5f5615e5cef6c138ff3ac015648086caae
Gabriel Copa <gabrielcncopa@gmail.com>
2023-10-27 04:20:28 -0300

    modified package and tsconfig

--- back-actotal/index.ts
+++ back-actotal/index.ts
@@ -1,8 +1,9 @@
 import app from "./src/app";
+import "dotenv/config";
 import { sequelize } from "./src/config/config-database";
 import "./src/models/Persons";
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT || 3002, async () => {
     try {
 
         await sequelize.sync({ force: false });
